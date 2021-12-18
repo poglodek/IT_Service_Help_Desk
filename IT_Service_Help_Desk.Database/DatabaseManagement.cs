2849399aa1bf4e8c3744be4f94fba0a84e0b1ff5
poglodek <pawelogll@gmail.com>
2021-12-18 20:21:23 +0100

    query recived date in object

--- IT_Service_Help_Desk.Database/DatabaseManagement.cs
+++ IT_Service_Help_Desk.Database/DatabaseManagement.cs
@@ -1,13 +1,17 @@
-﻿using MySql.Data.MySqlClient;
+﻿using IT_Service_Help_Desk.Services.IServices;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
 
 namespace IT_Service_Help_Desk.Database;
 
 public class DatabaseManagement
 {
+    private readonly ILogger _logger;
     private readonly MySqlConnection _mySqlConnection;
 
-    public DatabaseManagement(DatabaseConnector databaseConnector)
+    public DatabaseManagement(DatabaseConnector databaseConnector, ILogger logger)
     {
+        _logger = logger;
         _mySqlConnection = databaseConnector.GetConnection();
     }
 
@@ -24,8 +28,59 @@ public class DatabaseManagement
         }
         catch 
         {
-            Console.WriteLine("Error");
+            _logger.LogError($"Error in database query: {query}");
             return false;
+        }
+       
+    }
+    public List<T> GetResultsFromQuery<T>(string query)  
+    {
+        try
+        {
+            _mySqlConnection.Open();
+            var command = new MySqlCommand(query, _mySqlConnection);
+            var reader = command.ExecuteReader();
+            var list = new List<T>();
+            while (reader.Read())
+            {
+               // list.Add();
+            }
+            reader.Close();
+            _mySqlConnection.Close();
+            return list;
+        }
+        catch 
+        {
+            _logger.LogError($"Error in database query: {query}");
+            return null;
+        }
+       
+    }
+    public T GetResultFromQuery<T>(string query) where T : class
+    {
+        try
+        {
+            _mySqlConnection.Open();
+            var command = new MySqlCommand(query, _mySqlConnection);
+            var reader = command.ExecuteReader();
+            string sqlText = @"{";
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                   sqlText += $"\"{reader.GetName(i)}\": \"{reader[i]}\",";
+                }
+            }
+            var json = sqlText.Substring(0, sqlText.Length - 1);
+            json += "}";
+            reader.Close();
+            _mySqlConnection.Close();
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+        catch 
+        {
+            _logger.LogError($"Error in database query: {query}");
+            return null;
         }
        
     }
---
+++ IT_Service_Help_Desk.Test/Database/DatabaseManagement_Test.cs
@@ -0,0 +1,20 @@
+﻿using Castle.Core.Logging;
+using IT_Service_Help_Desk.Database;
+using IT_Service_Help_Desk.Database.Entity;
+using IT_Service_Help_Desk.Services.Services;
+using Moq;
+using Xunit;
+
+namespace IT_Service_Help_Desk.Test.Database;
+
+public class DatabaseManagement_Test
+{
+    [Fact]
+    public void GetResultFromQuery_CorrectQuery_ReturnObject()
+    {
+        var management = new DatabaseManagement(new DatabaseConnector(), new Logger());
+        var role = management.GetResultFromQuery<Role>($"SELECT * FROM roles LIMIT 1;");
+        Assert.NotNull(role);
+
+    }
+}
