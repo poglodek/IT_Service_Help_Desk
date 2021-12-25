e891dc8f92031f21180bb4496b256d4c0473ed96
poglodek <pawelogll@gmail.com>
2021-12-25 18:05:01 +0100

    Adding email validator test

---
+++ IT_Service_Help_Desk.Test/Validator/Helpers/EmailValidator_Test.cs
@@ -0,0 +1,34 @@
+﻿using IT_Service_Help_Desk.Validator.Helpers;
+using Xunit;
+
+namespace IT_Service_Help_Desk.Test.Validator.Helpers;
+
+public class EmailValidator_Test
+{
+    [Fact]
+    public void IsValidEmail_ValidEmail_ReturnsTrue()
+    {
+        // Arrange
+        var emailValidator = new EmailValidator();
+        var email = "test@test.com";
+        // Act
+        var result = emailValidator.IsValidEmail(email);
+        // Assert
+        Assert.True(result);
+    }
+    [Theory]
+    [InlineData("test!test.com")]
+    [InlineData("test@")]
+    [InlineData("@test@")]
+    [InlineData("est@@")]
+    [InlineData("@test.com")]
+    public void IsValidEmail_InvalidEmail_ReturnsFalse(string email)
+    {
+        // Arrange
+        var emailValidator = new EmailValidator();
+        // Act
+        var result = emailValidator.IsValidEmail(email);
+        // Assert
+        Assert.False(result);
+    }
+}
