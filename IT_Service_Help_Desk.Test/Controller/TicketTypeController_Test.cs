fdbe45e290206df584861aa7437e2b5d7e828d9e
poglodek <pawelogll@gmail.com>
2021-12-30 20:20:03 +0100

    adding tests for ticket type controller

---
+++ IT_Service_Help_Desk.Test/Controller/TicketTypeController_Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace IT_Service_Help_Desk.Test.Controller;
+
+public class TicketTypeController_Test : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+    public TicketTypeController_Test(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+    [Fact]
+    public void RegisterUser_InValidRegisterDto_ReturnFalse()
+    {
+        //Arrange
+            
+        //Act
+        var response = _client.GetAsync("/TicketType").Result;
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+            
+    }
+}
